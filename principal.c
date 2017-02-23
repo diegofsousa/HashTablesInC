ee178e9563f604f60e8e255df7718a304d5be1b9
diegofsousa <diegofernando5672@gmail.com>
2017-02-23 17:33:40 -0300

    Setando função para redimensionar se os indices prencher 3/4 dos espaços hash

--- principal.c
+++ principal.c
@@ -28,9 +28,10 @@ void hsl_busca(Hash *tab, int mat);
 
 int main(){
     Hash *hash = hsl_cria();
-    hash = hls_insere(hash, 15, "diego", "diego@gmail.com", '1');
-    hash = hls_insere(hash, 16, "qualquercoisa", "qualquercoisa@gmail.com", '2');
-    hsl_busca(hash, 16);
+    hash = hls_insere(hash, 336, "diego", "diego@gmail.com", '1');
+    hash = hls_insere(hash, 337, "qualquercoisa", "qualquercoisa@gmail.com", '2');
+    hash = hls_insere(hash, 339, "qualquercoisaoutra", "qualquercoisa@gmail.com", '1');
+    //hsl_busca(hash, 337);
 
 
     return 0;
@@ -40,7 +41,7 @@ int main(){
 //letra a
 Hash *hsl_cria(void){
     Hash *a = (Hash*)malloc(sizeof(Hash));
-    a->dim = 50;
+    a->dim = 4;
     a->n = 0;
     int i=0;
 
@@ -55,16 +56,36 @@ Hash *hsl_cria(void){
 
 //letra b
 int hash(Hash *tab, int mat){
-    return(mat%tab->dim);
+    printf("\nmatricula %d / dimensão %d", mat, tab->dim);
+    printf("\nHash gerado: %d", mat%tab->dim);
+    return mat%tab->dim;
 }
 
 //letra c
 void redimensiona(Hash* tab){
-    float porc = tab->n/tab->dim ;
-    if(porc < 0.75 ){
+    float porc = (float)tab->n/tab->dim;
+    printf("\ntab: %d e dim %d", tab->n, tab->dim);
+    printf("\nporc: %f", porc);
+    int i;
+    if(porc >= 0.75 ){
+        printf("\nPrecisou redimensionar");
         tab->v =(Aluno*)realloc(tab->v, 50*sizeof(Hash));
+        int auxDim = tab->dim;
         tab->dim += 50;
-        //remapear aqui
+        int tamAux = 0;
+        for(i=0;i<auxDim;i++){
+            if(tab->v[i] != NULL){
+                tamAux++;
+            }
+        }
+        int ialuno = 0;
+        Aluno aluno[tamAux];
+        for(i=0;i<auxDim;i++){
+            if(tab->v[i] != NULL){
+                aluno[ialuno] = *tab->v[i];
+            }
+            ialuno++;
+        }
     }
 }
 
@@ -83,16 +104,19 @@ Hash *hls_insere(Hash *tab, int mat, char *n, char *e, char t){
     strcpy(tab->v[h]->nome, n);
     strcpy(tab->v[h]->email, e);
     tab->v[h]->turma = t;
-    tab->n++;
+    tab->n += 1;
+
+    redimensiona(tab);
     return tab;
 }
 
 void hsl_busca(Hash *tab, int mat){
-    if(tab->v[mat] != NULL){
+    int h = hash(tab, mat);
+    if(tab->v[h] != NULL){
         printf("\nExiste!");
-        printf("\n%d\n%s\n%s\n%c", tab->v[mat]->mat, tab->v[mat]->nome, tab->v[mat]->email, tab->v[mat]->turma);
+        printf("\n%d\n%s\n%s\n%c", tab->v[h]->mat, tab->v[h]->nome, tab->v[h]->email, tab->v[h]->turma);
     }else{
-        printf("Não existe");
+        printf("\nNão existe");
     }
 }
 
