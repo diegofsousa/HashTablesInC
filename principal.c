b079fd3c99824d77be4ec1788d0ffbfa9778f083
diegofsousa <diegofernando5672@gmail.com>
2017-02-23 13:33:21 -0300

    estrutura criada

--- principal.c
+++ principal.c
@@ -1,1 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+typedef struct hash Hash;
+typedef struct aluno Aluno;
+struct hash{
+    int n;
+    int dim;
+    Aluno **v;
+};
+
+struct aluno{
+    int mat;
+    char nome[80];
+    char email[43];
+    char turma;
+};
+
+int main(){
+
+    return 0;
+}
